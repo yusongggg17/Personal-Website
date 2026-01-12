f7cff10722e0166165a684fcccb0ebd538d63c7c
Yusong Liang <1870502755lys@gmail.com>
2026-01-11 20:47:05 -0500

    added main page and profile page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import './App.css';
-
+//import files
+import Headshot from './assets/Professional_Headshot_Yusong_Liang_croped.jpeg';
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to My Personal Website</h1>
-    </div>
+    <body>
+      <div className="Title">
+        <h1>Yusong Liang's Personal Website</h1>
+      </div>
+      
+      <div>
+        <img className="profile-image" src={Headshot} alt="Headshot" />
+        <p className="Introduction">
+          My name is Yusong Liang,
+          I am a student at the Florida State University.
+          I am a Computer Science student.
+        </p>
+      </div>
+
+
+    </body>
   )
 }
+
+
+
 
 export default App;
