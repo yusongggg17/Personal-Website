6905012dd9b2233e67a736055c85460fdc135629
Yusong Liang <1870502755lys@gmail.com>
2026-01-13 16:35:18 -0500

    added contact section

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { FaEnvelope, FaGithub, FaLinkedin , FaItchIo} from "react-icons/fa";
 //import files
 import Headshot from './assets/Professional_Headshot_Yusong_Liang_croped.jpeg';
 function FrontPage() {
@@ -12,7 +13,7 @@ function FrontPage() {
 
 function Profile(){
   return(  
-    <div className="ProfileSection">
+    <div className="LighterSection">
       <h2 className="oval-bg-dark">About Me</h2>
       <img className="profileImage" src={Headshot} alt="Headshot" />
       <p className="p1">
@@ -50,12 +51,55 @@ function Projects(){
   );
 }
 
+function Contact(){
+  return(
+    <div className="LighterSection">
+      <h2 className="oval-bg-dark">Contact Me</h2>
+      <a href="mailto:yl23bd@fsu.edu"
+      className="oval-bg-dark" 
+      >
+        <p className="p2">Email Me!</p>
+      </a>
+      <div>
+        <a 
+          href="https://github.com/yusongggg17"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="AppButtons"
+        >
+          {FaGithub ({size:48})}
+        </a>
+
+        <a 
+          href="https://www.linkedin.com/in/yusong-liang/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="AppButtons"
+        >
+          {FaLinkedin ({size:48})}
+        </a>
+
+        <a 
+          href="https://yusongl.itch.io/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="AppButtons"
+        >
+          {FaItchIo ({size:48})}
+        </a>
+
+      </div>
+    </div>
+  );
+
+}
 function App() {
   return (
     <>
       <FrontPage />
       <Profile />
       <Projects />
+      <Contact/>
 
     </>
   );
